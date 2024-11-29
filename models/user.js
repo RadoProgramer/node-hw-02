03784e20f79efabc4be6c5febeb430b26784ebdb
RadoProgramer <145141434+RadoProgramer@users.noreply.github.com>
2024-11-29 10:03:50 +0000

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,52 +1,52 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-const gravatar = require("gravatar");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const gravatar = require('gravatar');
 
 const userSchema = new mongoose.Schema({
-	email: {
-		type: String,
-		required: [true, "Email is required"],
-		unique: true,
-		match: [/.+@.+\..+/, "Please enter a valid email address"],
-	},
-	password: {
-		type: String,
-		required: [true, "Password is required"],
-		minlength: [6, "Password must be at least 6 characters long"],
-	},
-	subscription: {
-		type: String,
-		enum: ["starter", "pro", "business"],
-		default: "starter",
-	},
-	token: {
-		type: String,
-		default: null,
-	},
-	avatarURL: {
-		type: String,
-		default: function () {
-			return gravatar.url(this.email, { s: "250", d: "retro" }, true);
-		},
-	},
-	verify: {
-		type: Boolean,
-		default: false,
-	},
-	verificationToken: {
-		type: String,
-		required: [true, "Verify token is required"],
-	},
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    match: [/.+@.+\..+/, 'Please enter a valid email address'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [6, 'Password must be at least 6 characters long'],
+  },
+  subscription: {
+    type: String,
+    enum: ['starter', 'pro', 'business'],
+    default: 'starter',
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+  avatarURL: {
+    type: String,
+    default: function () {
+      return gravatar.url(this.email, { s: '250', d: 'retro' }, true);
+    },
+  },
+  verify: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+    default: null,
+  },
 });
 
 userSchema.methods.setPassword = async function (password) {
-	this.password = await bcrypt.hash(password, 10);
+  this.password = await bcrypt.hash(password, 10);
 };
 
 userSchema.methods.validatePassword = async function (password) {
-	return await bcrypt.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 };
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
